a84eabb85f1dca43d9e14a2516f5e1c0d9ac1ee2
Simao Nziaka <sdnziaka@gmail.com>
2018-07-30 04:33:45 -0600

    Added lo-fi update  - rms old scripts and cps  new scripts

--- install.sh
+++ install.sh
@@ -1,39 +1,76 @@
 #!bin/bash
 
+#the instalation folder path
+INSTALL_DIR="$HOME/.bashtian";
+
+#setup the path of the scripts
+gitit_path="$INSTALL_DIR/gitit.sh";
+full_update_path="$INSTALL_DIR/full_update.sh";
+
 #install only if does not exist
 if [ ! -e "$HOME/.bashtian" ]
 then
     #create the instalation folder
-    INSTALL_DIR="$HOME/.bashtian";
     mkdir -p "$INSTALL_DIR";
 
     #copy shell scripts to the install directory
     cp gitit.sh "$INSTALL_DIR";
     cp full_update.sh "$INSTALL_DIR";
 
-    #setup the aliases
-    gitit_alias="$INSTALL_DIR/gitit.sh";
-    full_update_alias="$INSTALL_DIR/full_update.sh";
-
-    #check if bashrc exists and append the aliases
+    #check if bashrc exists and append aliases
     if [ -f "$HOME/.bashrc" ]
     then
         APPEND_RC="$HOME/.bashrc";
         echo "#Bashtian aliases" >> "$APPEND_RC";
-        echo "alias gitit='$gitit_alias'" >> "$APPEND_RC";
-        echo "alias full-update='$full_update_alias'" >> "$APPEND_RC";
+        echo "alias gitit='$gitit_path'" >> "$APPEND_RC";
+        echo "alias full-update='$full_update_path'" >> "$APPEND_RC";
     fi
 
-    #check if zshrc exists and append the aliases
+    #check if zshrc exists and append aliases
     if [ -f "$HOME/.zshrc" ]
     then
         APPEND_RC="$HOME/.zshrc"
         echo "#Bashtian aliases" >> "$APPEND_RC";
-        echo "alias gitit='$gitit_alias'" >> "$APPEND_RC";
-        echo "alias full-update='$full_update_alias'" >> "$APPEND_RC";
+        echo "alias gitit='$gitit_path'" >> "$APPEND_RC";
+        echo "alias full-update='$full_update_path'" >> "$APPEND_RC";
     fi
 
     #refresh the rc file of the current shell
     if [ "$SHELL" = "/bin/zsh" ]; then source "$HOME/.zshrc"; fi
     if [ "$SHELL" = "bash" ]; then source "$HOME/.bashrc"; fi
+
+echo '                                                          ';
+echo ' _______                                                  ';
+echo '|   _   \ ______                                          ';
+echo '|  | |  /   _   |           _______          __     _     ';
+echo '|  |_| |   | |  |______     \__    _\ _     |  |   | |    ';
+echo '|   _   \  |_|  |  ____| ___|  |  |  | |____|   \__| |    ';
+echo '|  | |   |  _   | [____  ___   |  |  | |  _ \        |    ';
+echo '|  |_|   / | |  |   O  |    |  |  |  | | |_| |   _   |    ';
+echo '|______ /  | |  |  ____/    |  |  |  |_|   __|  | |__|    ';
+echo '';
+echo '';
+echo 'Installed Successfully!';
+else
+    #if it exists, update
+    #first remove the existing scripts
+    rm "$gitit_path";
+    rm "$full_update_path";
+
+    #copy the new shell scripts to the install directory
+    cp gitit.sh "$INSTALL_DIR";
+    cp full_update.sh "$INSTALL_DIR";
+
+echo '                                                          ';
+echo ' _______                                                  ';
+echo '|   _   \ ______                                          ';
+echo '|  | |  /   _   |           _______          __     _     ';
+echo '|  |_| |   | |  |______     \__    _\ _     |  |   | |    ';
+echo '|   _   \  |_|  |  ____| ___|  |  |  | |____|   \__| |    ';
+echo '|  | |   |  _   | [____  ___   |  |  | |  _ \        |    ';
+echo '|  |_|   / | |  |   O  |    |  |  |  | | |_| |   _   |    ';
+echo '|______ /  | |  |  ____/    |  |  |  |_|   __|  | |__|    ';
+echo '';
+echo '';
+echo 'Updated Successfully!';
 fi
